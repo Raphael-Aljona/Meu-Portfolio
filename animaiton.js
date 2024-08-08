7856aadb9d368ec65fc43735b49e8fd3b41a0045
Raphael-Aljona <raphael.aljona@gmail.com>
2024-08-08 16:12:11 -0300

    feat: Implementação de animação

---
+++ animaiton.js
@@ -0,0 +1,21 @@
+
+// Animação para fazer ao scrollar a página
+
+const elements = document.querySelectorAll('.hidden');
+
+const myObserver = new IntersectionObserver((elements) => { 
+    elements.forEach(( entry ) => {
+        console.log(entry.target)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else{
+            entry.target.classList.remove('show')
+        }
+        
+        })
+})
+
+elements.forEach((element) => myObserver.observe(element))
+
+
+
